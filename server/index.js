edff0874f0231581e768107f399fbd7ecafa316c
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-04-25 11:10:58 -0700

    Express server now serves application. #1

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 require('dotenv/config')
 const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
 
 const app = express()
+const jsonParser = bodyParser.json()
+const publicPath = path.join(__dirname, 'public')
+const staticMiddleware = express.static(publicPath)
 
-app.get('/', (req, res) => {
-  res.send('Server has been set up.')
-})
+app.use(jsonParser)
+app.use(staticMiddleware)
 
 app.listen(process.env.PORT, () => {
   console.log('Listening on port', process.env.PORT)
