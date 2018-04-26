8a3fafcd6b2cc46edcd56210e4640e0b0a5ceced
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-04-26 10:07:57 -0700

    Added search bar functionality. #1

--- client/components/search.js
+++ client/components/search.js
@@ -1,15 +1,53 @@
 import React from 'react'
 
 export default class Search extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      summoner: null,
+      foundSummoner: null,
+      summonerLevel: null
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.handleInput = this.handleInput.bind(this)
+  }
+  handleClick() {
+    fetch('/search?name=' + this.state.summoner)
+      .then(res => res.json())
+      .then(summoner =>
+        this.setState({
+          foundSummoner: summoner.name,
+          summonerLevel: summoner.summonerLevel
+        })
+      )
+  }
+  handleInput(event) {
+    this.setState({ summoner: event.target.value })
+  }
   render() {
     return (
-      <div className="input-group mt-5">
-        <input type="text" className="form-control" placeholder="Summoner" />
-        <div className="input-group-append">
-          <button className="btn btn-outline-secondary" type="button">
-            Button
-          </button>
+      <div>
+        <div className="input-group mt-5">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Summoner"
+            onInput={this.handleInput}
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              onClick={this.handleClick}
+            >
+              Search
+            </button>
+          </div>
         </div>
+        <ul>
+          <li>Summoner Name: {this.state.foundSummoner}</li>
+          <li>Summoner Level: {this.state.summonerLevel}</li>
+        </ul>
       </div>
     )
   }
