66578edd606750f780074fcef907632d7675e186
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-05-03 10:48:27 -0700

    Combined error handling with express middleware. #5

--- server/create-app.js
+++ server/create-app.js
@@ -16,65 +16,49 @@ module.exports = function createApp() {
   app.use(jsonParser)
   app.use(staticMiddleware)
 
-  app.get('/search', (req, res) => {
+  app.get('/search', (req, res, next) => {
     const name = req.query.name
     getSummoner(name, (err, summoner) => {
-      if (err) {
-        res.sendStatus(500)
-        console.error(err)
-        return err
-      }
+      if (err) return next(err)
       res.json(summoner)
     })
   })
 
-  app.get('/rank', (req, res) => {
+  app.get('/rank', (req, res, next) => {
     const id = req.query.id
     getRank(id, (err, rank) => {
-      if (err) {
-        res.sendStatus(500)
-        console.error(err)
-        return err
-      }
+      if (err) return next(err)
       res.json(rank)
     })
   })
 
-  app.get('/matches', (req, res) => {
+  app.get('/matches', (req, res, next) => {
     const id = req.query.id
     getMatchHistory(id, (err, matches) => {
-      if (err) {
-        res.sendStatus(500)
-        console.error(err)
-        return err
-      }
+      if (err) return next(err)
       res.json(matches)
     })
   })
 
-  app.get('/match', (req, res) => {
+  app.get('/match', (req, res, next) => {
     const id = req.query.id
     getMatch(id, (err, match) => {
-      if (err) {
-        res.sendStatus(500)
-        console.error(err)
-        return err
-      }
+      if (err) return next(err)
       res.json(match)
     })
   })
 
-  app.get('/icon', (req, res) => {
+  app.get('/icon', (req, res, next) => {
     const id = req.query.id
     getIcon(id, (err, icon) => {
-      if (err) {
-        res.sendStatus(500)
-        console.error(err)
-        return err
-      }
+      if (err) return next(err)
       res.json(icon)
     })
   })
 
+  app.use((err, req, res, next) => {
+    res.sendStatus(500)
+    console.error(err)
+  })
   return app
 }
