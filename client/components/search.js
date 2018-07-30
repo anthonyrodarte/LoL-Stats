b7ee7253dcea2b20452043fc188b6e53a0ca358a
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-07-30 13:09:58 -0700

    Fixed banner images, improved search bar. #16

--- client/components/search.js
+++ client/components/search.js
@@ -3,22 +3,20 @@ import { Button, Input, InputGroup, InputGroupAddon } from 'reactstrap'
 
 export default function Search(props) {
   return (
-    <div>
-      <InputGroup>
-        <Input
-          type="text"
-          placeholder="Summoner"
-          onInput={props.input}
-        />
-        <InputGroupAddon addonType="append">
-          <Button
-            outline color="secondary"
-            onClick={props.click}
-          >
-            Search
-          </Button>
-        </InputGroupAddon>
-      </InputGroup>
-    </div>
+    <InputGroup>
+      <Input
+        type="text"
+        placeholder="Summoner"
+        onInput={props.input}
+      />
+      <InputGroupAddon addonType="append">
+        <Button
+          outline color="secondary"
+          onClick={props.click}
+        >
+          Search
+        </Button>
+      </InputGroupAddon>
+    </InputGroup>
   )
 }
